a9cc892203f90f2ff3207482f0d313011c953611
Hin Hui <hin.hui@esi-asia.com>
2024-07-13 14:30:54 +0100

    [NewFeat] add Logout

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -60,7 +60,7 @@ export const login = async(req, res) => {
         if (!user || !isPasswordCorrect) {
 			return res.status(400).json({ error: "Invalid username or password" });
 		}
-        
+
         generateTokenAndSetCookie(user._id, res);
 
         res.status(200).json({
@@ -77,5 +77,11 @@ export const login = async(req, res) => {
 }
 
 export const logout = (req, res) => {
-    
+    try {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logged out successfully" });
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 }
