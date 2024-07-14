0bb2f7157a3f75b15f7f6b2b58d91946bd2d3dd0
Hin Hui <hin.hui@esi-asia.com>
2024-07-14 13:19:49 +0100

    [Backend][NewFeat] Add Users

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRouters from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRouters);
 app.use("/api/messages", messageRoutes);
-//app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
