144506625b26a94348b34280b28ee21692ac040a
Hin Hui <hin.hui@esi-asia.com>
2024-07-15 15:34:29 +0100

    [Front][UpdateFeat] Signup submit to backend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,14 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{ port: 3000,},
+  server:{ 
+    port: 3000,
+    proxy:{
+      "/api": {
+				target: "http://localhost:8000",
+			},
+    }
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
