42fec2d1922a852984c0e959ee53115beba261fb
Hin Hui <hin.hui@esi-asia.com>
2024-07-16 18:55:21 +0100

    add deploy command

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,14 +10,22 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body) 
+app.use(express.json()); 
 app.use(cookieParser());
 
 app.use("/api/auth", authRouters);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
