91aa13f9152c9a3491a0a0d2ebfa0ac870b3b273
Hin Hui <hin.hui@esi-asia.com>
2024-07-16 18:28:39 +0100

    [Front][Back][UpdateFeat] change use socket; conversation list show online;

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ import authRouters from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-const app = express();
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -18,7 +18,7 @@ app.use("/api/auth", authRouters);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log('Server Running on port ' + PORT);
 }
