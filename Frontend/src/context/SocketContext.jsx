bd3b9a4ff14ac24bd031d1ba6bdbe8d148d07140
Hin Hui <hin.hui@esi-asia.com>
2024-07-16 20:58:16 +0100

    [Front] fix useSocketContext

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -1,25 +1,41 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import { useAuthContext } from "./AuthContext";
-import io from "socket.io-client"
+import io from "socket.io-client";
 
 const SocketContext = createContext();
 
-export const SocketContextProvider = ({ children }) => {
-    const [socket, setSocket] = useState(null);
-	const [onlineUsers, setOnlineUsers] = useState([]);
-    const { authUser } = useAuthContext();
-    useEffect(() => {
-        if (authUser) {
-            const socket = io(import.meta.env.VITE_SOCKET_IO_URL)
-            setSocket(socket);
+export const useSocketContext = () => {
+	return useContext(SocketContext);
+};
 
-            return () => socket.close();
-        }else{
-            if (socket) {
+export const SocketContextProvider = ({ children }) => {
+	const [socket, setSocket] = useState(null);
+	const [onlineUsers, setOnlineUsers] = useState([]);
+	const { authUser } = useAuthContext();
+
+	useEffect(() => {
+		if (authUser) {
+			const socket = io(process.env.SOCKET_IO_URL, {
+				query: {
+					userId: authUser._id,
+				},
+			});
+
+			setSocket(socket);
+
+			// socket.on() is used to listen to the events. can be used both on client and server side
+			socket.on("getOnlineUsers", (users) => {
+				setOnlineUsers(users);
+			});
+
+			return () => socket.close();
+		} else {
+			if (socket) {
 				socket.close();
 				setSocket(null);
 			}
-        }
-    });
-    return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
+		}
+	}, [authUser]);
+
+	return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
 };
