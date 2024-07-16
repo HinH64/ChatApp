5efc18343aeadcf7bd1c9cb19e89ac9ca481ad4e
Hin Hui <hin.hui@esi-asia.com>
2024-07-16 12:31:28 +0100

    [Front] add select Conversation function for sidebar; add send message function

--- Frontend/src/components/messages/MessageContainer.jsx
+++ Frontend/src/components/messages/MessageContainer.jsx
@@ -1,21 +1,28 @@
 import MessageList from "./MessageList"
 import MessageInput from "./MessageInput"
 import { TiMessages } from "react-icons/ti";
-
+import useConversation from "../../zustand/useConversation";
+import { useEffect } from "react";
 
 const MessageContainer = () => {
-    const selectedConversation = false;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+		// cleanup function (unmounts)
+		return () => setSelectedConversation(null);
+	}, [setSelectedConversation]);
+
   return (
     <div className='md:min-w-[450px] flex flex-col'>
         {!selectedConversation ? (
 				<NoChatSelected />
 			) : (
                 <>
-                 <div className='bg-slate-500 px-4 py-2 mb-2'>
-                    <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
-                </div>
-                <MessageList />
-                <MessageInput />
+                  <div className='bg-slate-500 px-4 py-2 mb-2'>
+                      <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+                  </div>
+                  <MessageList />
+                  <MessageInput />
                 </>
             )
         }
