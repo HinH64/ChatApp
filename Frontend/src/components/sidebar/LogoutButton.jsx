83da7a067ef0976120001c5b9178caf735abe77c
Hin Hui <hin.hui@esi-asia.com>
2024-07-17 17:51:44 +0100

    [Front][UI]change logout button

--- Frontend/src/components/sidebar/LogoutButton.jsx
+++ Frontend/src/components/sidebar/LogoutButton.jsx
@@ -3,7 +3,7 @@ import useLogout from "../../hooks/useLogout";
 const LogoutButton = () => {
     const { loading, logout } = useLogout();
   return (
-    <div className='mt-auto'>
+    <div >
         {!loading ? (
              <BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout}/>
         ):(
--- Frontend/src/components/sidebar/Sidebar.jsx
+++ Frontend/src/components/sidebar/Sidebar.jsx
@@ -10,12 +10,13 @@ const Sidebar = () => {
     <div className='border-r border-slate-500 p-4 flex flex-col'>
 			<div className='flex gap-2 items-center rounded p-2 py-1 '>
 			<UserInfo userData={authUser} isOnline={false} />
+			<LogoutButton />
 			</div>
 			<div className='divider px-3'></div>
 			<SearchInput />
 			<div className='divider px-3'></div>
 			<ConversationList />
-			<LogoutButton />
+			
 		</div>
   )
 }
